c621c939a76b321602bc53d416f5fcab6d3dd5c5
Thao Stein <thaostein1@gmail.com>
2023-12-28 19:17:45 +0100

    changed app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,13 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <main>
+    <StellenangebotForm title="Stellenangebote verwalten" />
+  </main>
 </template>
 
+<script setup lang="ts">
+import StellenangebotForm from './components/StellenangebotForm.vue'
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 14px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
+/* Hier können spezifische Stile für die HomeView hinzugefügt werden */
 </style>
