ef55cc70e1f206f8976552c4746feeb7b0064b3f
Thao Stein <thaostein1@gmail.com>
2024-01-04 19:15:03 +0100

    created FooterComponent.vue

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer class="footer">
+    <div class="footer-content">
+      <div class="copyright">© 2023 Kathao, GmbH.</div>
+      <nav class="footer-nav">
+        <router-link to="/terms">Terms</router-link>
+        <router-link to="/privacy">Privacy</router-link>
+        <router-link to="/security">Security</router-link>
+        <router-link to="/contact">Contact</router-link>
+        <router-link to="/manage-cookies">Manage cookies</router-link>
+      </nav>
+    </div>
+  </footer>
+</template>
+
+
+<style scoped>
+.footer {
+  background-color: white;
+  color: black;
+  text-align: center;
+  padding: 20px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+
+.footer-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: -60px;
+}
+
+.footer-nav {
+  display: flex;
+  gap: 20px;
+  margin-right: 140px;
+}
+
+.footer-nav a {
+  color: black;
+  text-decoration: none;
+}
+
+.manage-cookies {
+  font-size: 14px;
+}
+</style>
+<script setup lang="ts">
+</script>
