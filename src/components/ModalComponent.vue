81786cf9c8c27d26d9dd6cb1d24d41299e6fbfb3
Thao Stein <thaostein1@gmail.com>
2024-01-10 21:50:57 +0100

    added ModalComponent.vue

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="modal" v-if="show">
+    <div class="modal-content">
+      <slot></slot>
+      <button class="close-button" @click="closeModal">&times;</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  show: Boolean,
+});
+
+const emit = defineEmits(['update:show']);
+
+const closeModal = () => {
+  emit('update:show', false);
+};
+</script>
+
+<style scoped>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0,0,0,0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close-button {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close-button:hover,
+.close-button:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
